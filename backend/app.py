ea4ecc94a7f1e461a836e997fc35fdc3ccf55d5a
maanvi shadakshari <maanvishadakshari21@gmail.com>
2026-04-26 13:33:19 +0530

    rem client

--- backend/app.py
+++ backend/app.py
@@ -149,5 +149,5 @@ async def upload_excel(file: UploadFile = File(...)):
 
 if __name__ == "__main__":
     import uvicorn
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("pumpsense-production.up.railway.app", 8000))
     uvicorn.run("app:app", host="0.0.0.0", port=port)
